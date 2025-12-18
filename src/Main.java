762f1d31d9aaf1aee2d0d7bce9cb4e8543dd5290
user <Hajbullini2000@gmail.com>
2025-12-19 00:16:04 +0500

    Class_work_№1  изучил переменные и как присваивается имя и значение переменным

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        System.out.println("dog = " + dog );
+        var cat = 3.6;
+        System.out.println("cat = " + cat);
+        var paper = 763789.0;
+        System.out.println("paper = " + paper);
+
+        var addition_dog = dog + 4;
+        System.out.println("dog + 4 = addition_dog =  " + addition_dog);
+        var addition_cat = cat + 4;
+        System.out.println("cat + 4 = addition_cat = " + addition_cat);
+        var addition_paper = paper + 4;
+        System.out.println("paper + 4 = addition_paper = " + addition_paper);
+
+        var subtraction_dog = addition_dog - 3.5;
+        System.out.println("addition_dog - 3.5 = subtraction_dog = " + subtraction_dog);
+        var subtraction_cat = addition_cat - 1.6;
+        System.out.println("addition_cat - 1.6 = subtraction_cat = " + subtraction_cat);
+        var subtraction_paper = addition_paper - 7639;
+        System.out.println("addition_paper - 7639 = subtraction_paper = " + subtraction_paper);
+
+        var friend = 19;
+        System.out.println("переменная friend = " + friend);
+        friend = friend + 2;
+        System.out.println("переменная friend = " + friend);
+        friend = friend / 7;
+        System.out.println("переменная friend = " + friend);
+
+        var frog = 3.5;
+        System.out.println("переменная frog = " + frog);
+        frog = frog * 10;
+        System.out.println("переменная frog = " + frog);
+        frog = frog / 3.5;
+        System.out.println("переменная frog = " + frog);
+        frog = frog + 4;
+        System.out.println("переменная frog = " + frog);
+
+        var 
     }
 }
