4f4f1003168c49ae2c3e0f63526444473bf3f5ac
user <Hajbullini2000@gmail.com>
2026-02-18 21:23:12 +0500

    Home_work_№8 выполнил задания по теме массивы ч-1(исправление - финальное)

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
         int[] arrayOne = new int[3];
-        for (int index = 0; index < arrayOne.length; index++){
+        for (int index = 0; index < arrayOne.length; index++) {
             arrayOne[index] = index + 1;
             System.out.println(arrayOne[index]);
         }
         double[] arrayTwo = {1.57, 7.654, 9.986};
         for (double index : arrayTwo) {
-            System.out.println(index+arrayTwo.length);
+            System.out.println(index + arrayTwo.length);
         }
         double[] arrayThree = new double[5];
         for (int index = 0; index < arrayThree.length; index++) {
@@ -19,47 +19,44 @@ public class Main {
         }
         System.out.println("=============================");
         System.out.println("Задание 2");
-        System.out.println(Arrays.toString(arrayOne)+" ");
-        System.out.println(Arrays.toString(arrayTwo)+" ");
-        System.out.println(Arrays.toString(arrayThree)+"  ");
+        System.out.println(Arrays.toString(arrayOne) + " ");
+        System.out.println(Arrays.toString(arrayTwo) + " ");
+        System.out.println(Arrays.toString(arrayThree) + "  ");
 
         System.out.println("=============================");
         System.out.println("Задание 3");
-        int length = 0;
-        for (int ignored : arrayOne) {
-            length++;}
-        for (int i = length - 1; i >= 0; i--) {
+        for (int i = arrayOne.length - 1; i >= 0; i--) {
             System.out.print(arrayOne[i]);
             if (i > 0) {
-                System.out.print(", ");}
+                System.out.print(", ");
+            }
         }
         System.out.println();
 
-        for (double ignored : arrayTwo) {
-            length++;}
-        for (int a = length - 4; a >= 0; a--) {
-            System.out.print(arrayTwo[a]);
-            if (a > 0) {
-                System.out.print(", ");}
+
+        for (int i = arrayTwo.length - 1; i >= 0; i--) {
+            System.out.print(arrayTwo[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println("   ");
 
-        for (double ignored : arrayThree) {
-            length++;}
-        for (int i = length - 7; i >= 0; i--) {
+        for (int i = arrayThree.length - 1; i >= 0; i--) {
             System.out.print(arrayThree[i]);
             if (i > 0) {
-                System.out.print(", ");}
+                System.out.print(", ");
+            }
         }
         System.out.println();
 
         System.out.println("===========================");
         System.out.println("Задание 4");
-        boolean theOddElement = true;
-        for (int i = 0; i < arrayOne.length; i++){
-            if (arrayOne[i] % 2 != 0){
+        for (int i = 0; i < arrayOne.length; i++) {
+            if (arrayOne[i] % 2 != 0) {
                 arrayOne[i]++;
-            }System.out.print(arrayOne[i]);
+            }
+            System.out.print(arrayOne[i] + " ");
         }
         System.out.println();
     }
